96e65991e3862445b3ec4ff7499e8bf32b413595
marioyc <ycmario@gmail.com>
2015-04-07 23:39:06 +0200

    get a random deck

--- src/Server.java
+++ src/Server.java
@@ -64,30 +64,7 @@ public class Server extends JFrame{
 			BufferedReader in2 = Net.connectionIn(s2);
 			PrintWriter out2 = Net.connectionOut(s2);
 			
-			ArrayList<Integer> deck = new ArrayList<Integer>();//Cards.generateDeck();
-			deck.add(102); deck.add(253); deck.add(223); deck.add(111); deck.add(254);
-			deck.add(231); deck.add(171); deck.add(213); deck.add(221); deck.add(250);
-			deck.add(109); deck.add(215); deck.add(183);
-			
-			deck.add(85); deck.add(118); deck.add(245); deck.add(103); deck.add(233);
-			deck.add(117); deck.add(229); deck.add(95); deck.add(246); deck.add(105);
-			deck.add(158); deck.add(91); deck.add(126);
-			
-			deck.add(122); deck.add(127); deck.add(237); deck.add(87); deck.add(153);
-			deck.add(123); deck.add(191); deck.add(94); deck.add(166); deck.add(106);
-			deck.add(189); deck.add(90); deck.add(151); deck.add(249);
-			
-			deck.add(121); deck.add(89); deck.add(187); deck.add(155); deck.add(181);
-			deck.add(174); deck.add(86); deck.add(190); deck.add(110); deck.add(185);
-			deck.add(235); deck.add(173); deck.add(165);
-			
-			deck.add(154); deck.add(251); deck.add(119); deck.add(222); deck.add(234);
-			deck.add(239); deck.add(214); deck.add(175); deck.add(149); deck.add(230);
-			deck.add(157); deck.add(219); deck.add(217);
-			
-			deck.add(182); deck.add(101); deck.add(255); deck.add(170); deck.add(150);
-			deck.add(167); deck.add(218); deck.add(169); deck.add(247); deck.add(93);
-			deck.add(159); deck.add(186); deck.add(238); deck.add(125); deck.add(107);
+			ArrayList<Integer> deck = Cards.generateDeck();
 			N = 12;
 			posDeck = 12;
 			
@@ -102,7 +79,7 @@ public class Server extends JFrame{
 			
 			deckString = deckString.substring(1, deckString.length());
 			
-			//write(deckString);
+			write(deckString);
 			out1.println(deckString);
 			out2.println(deckString);
 			
@@ -190,8 +167,7 @@ public class Server extends JFrame{
 					
 					N -= 3;
 					
-					if(N == 12)
-						cleanCards(N + 3);
+					cleanCards(N + 3);
 					
 					while(81 - posDeck >= 3 && (N < 12 || (N == 12 && !Cards.test(value, N)))){
         				//System.out.println("N = " + N);
@@ -206,28 +182,6 @@ public class Server extends JFrame{
 					if(!test(value,N)){
 						end = true;
 					}
-					
-					/*if((N == 9 && 81 - posDeck >= 3) || (N == 12 && !test(value,N))){
-        				for(int k = 0;k < 3;++k){
-        					value[ c[k] ] = deck.get(posDeck);
-        					posDeck++;
-        				}
-        				
-        				N += 3;
-        				
-        				if(!test(value,N) && 81 - posDeck >= 3){
-        					for(int k = 0;k < 3;++k){
-        						value[12 + k] = deck.get(posDeck);
-        						posDeck++;
-        					}
-        					
-        					N += 3;
-        				}
-        				
-    					if(!test(value,N)){
-    						end = true;
-    					}
-					}*/
 				}
 				
 				write("-------------------");
